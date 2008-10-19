5ad67bc30b5d0b72bf51c5f7f40842b1d6337d71
Anders Waldenborg <andersg@leonov.0x63.nu>
2008-10-19 20:14:35 +0200

    Add initial uzlib stuff.

---
+++ uztest.c
@@ -0,0 +1,60 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "uzlib.h"
+
+int
+main (int argc, char **argv)
+{
+	z_stream z;
+	unsigned char buf[4096];
+	unsigned char obuf[4096];
+	int ret, i;
+
+	z.zalloc = Z_NULL;
+	z.zfree = Z_NULL;
+	z.avail_in = 0;
+	z.next_in = Z_NULL;
+
+	if (inflateInit (&z) != Z_OK)
+		return 1;
+
+	z.next_in = buf;
+
+	for (;;) {
+		if (z.avail_in == 0)
+			z.avail_in = read (STDIN_FILENO, buf, sizeof (buf));
+		if (z.avail_in == -1) {
+			perror ("read");
+			exit (1);
+		}
+		if (z.avail_in == 0) {
+			printf ("Early EOF\n");
+			exit (1);
+		}
+		
+		z.next_out = obuf;
+		z.avail_out = sizeof (obuf);
+
+		ret = inflate (&z, 0);
+
+		if (ret == Z_STREAM_END)
+			break;
+		if (ret != Z_OK) {
+			printf ("Bad\n");
+			return 1;
+		}
+		for (i = 0; i < sizeof (buf) - z.avail_out; i++) {
+			printf ("0x%02x\n", obuf[i]);
+		}
+	}
+
+	for (i = 0; i < sizeof (buf) - z.avail_out; i++) {
+		printf ("0x%02x\n", obuf[i]);
+	}
+
+
+	return 0;
+}
