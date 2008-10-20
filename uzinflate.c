fe1afc938d528811184356a1267242f2b6aa26da
Anders Waldenborg <andersg@leonov.0x63.nu>
2008-10-20 10:16:57 +0200

    Add myself.

--- uzinflate.c
+++ uzinflate.c
@@ -5,6 +5,8 @@
 
   Copyright (C) 1995-2005 Jean-loup Gailly and Mark Adler
 
+  Slimed down by Anders Waldenborg 2008
+
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
   arising from the use of this software.
